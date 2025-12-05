c9f9cb91a963f86c258de48cebb956a24aea7c8d
Mateus Batista <mateusbatista0204@gmail.com>
2025-12-05 17:11:35 -0300

    Removendo arquivos desconexos

--- listadupla/ListaDupla.js
+++
@@ -1,9 +0,0 @@
-const SingleNode = require("../models/node");
-const Lista = require("../listaordenada/listaordenada");
-
-class DoublyLinkedList {
-  constructor(enqualsFn = defaultEnquals) {
-    this.list = new Lista(enqualsFn);
-    this.prev = null;
-  }
-}
